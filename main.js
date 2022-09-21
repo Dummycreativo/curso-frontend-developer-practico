dceef74540f97e4ef7e912e071e0f810aecaa5db
Joel Bozian <joelb@artisanvl.com>
2022-09-21 12:44:14 -0300

    Desktop Menu Js

--- main.js
+++ main.js
@@ -2,14 +2,37 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHambIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const cartDetail = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHambIcon.addEventListener('click', toggleMobileMenu);
+shoppingCartIcon.addEventListener('click', toggleCartDetail);
 
 function toggleDesktopMenu() {
+    const isCartDetailClosed = cartDetail.classList.contains('inactive');
+    if (!isCartDetailClosed){
+        cartDetail.classList.add('inactive'); 
+    }
     desktopMenu.classList.toggle('inactive'); 
 }
 
 function toggleMobileMenu() {
+    const isCartDetailClosed = cartDetail.classList.contains('inactive');
+
+    if (!isCartDetailClosed) {
+        cartDetail.classList.add('inactive'); 
+    }
     mobileMenu.classList.toggle('inactive'); 
+}
+
+function toggleCartDetail() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    // Is mobileMenu open? 
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+    cartDetail.classList.toggle('inactive'); 
 }
